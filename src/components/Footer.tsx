507fa947af968417c83a1c3c892048a50ec92b21
Godswill <godswilleguavoen@gmail.com>
2026-04-22 12:19:46 +0100

    added my logo to the website

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { motion } from "framer-motion";
-
+import Image from "next/image";
+import LogoImage from "../../public/GW-Teck_Solution-logo-transparent-removebg-preview.png";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className='relative py-16 px-6  overflow-hidden'>
+    <footer className='relative pt-16 px-6  overflow-hidden'>
       {/* Top Border Gradient - Creates a "shimmer" line at the top of the footer */}
       <div className='absolute top-0 left-1/2 -translate-x-1/2 w-full max-w-7xl h-[1px] bg-gradient-to-r from-transparent via-white/20 to-transparent' />
 
@@ -48,12 +49,12 @@ export default function Footer() {
             {/* These can be actual links or just stylistic placeholders */}
             <a
               href='#'
-              className='text-gray-600 hover:text-white text-xs transition-colors uppercase tracking-widest'>
+              className='text-gray-400 hover:text-white text-xs transition-colors uppercase tracking-widest'>
               Privacy Policy
             </a>
             <a
               href='#'
-              className='text-gray-600 hover:text-white text-xs transition-colors uppercase tracking-widest'>
+              className='text-gray-400 hover:text-white text-xs transition-colors uppercase tracking-widest'>
               Terms of Access
             </a>
           </div>
@@ -62,6 +63,23 @@ export default function Footer() {
         {/* Decorative corner glows to frame the bottom of the page */}
         <div className='absolute -bottom-10 -left-10 w-40 h-40 bg-[#c6a85a]/5 blur-3xl rounded-full pointer-events-none' />
         <div className='absolute -bottom-10 -right-10 w-40 h-40 bg-white/5 blur-3xl rounded-full pointer-events-none' />
+
+        {/* Powered by section */}
+        <div className=' flex flex-col md:flex-row items-center justify-center '>
+          <img
+            src='/GW-Teck_Solution-logo-transparent-removebg-preview.png'
+            alt='GW-Teck Solutions Logo'
+            className='w-40 h-auto' // adjust size as needed
+          />
+
+          <p className='text-gray-400 text-[10px]'>
+            Powered by <strong>GW-Teck Solutions</strong> | Contact:
+            {"+2348066168225"} |{" "}
+            <a href='godswilleguavoen@gmail.com' className='underline'>
+              Email Me
+            </a>
+          </p>
+        </div>
       </div>
     </footer>
   );
